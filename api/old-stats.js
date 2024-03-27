7fd109bff0efc3419147e92d18bde0e6f2ebb779
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-03-27 16:39:50 +0530

    fix

--- api/old-stats.js
+++ api/old-stats.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
@@ -8,8 +9,10 @@ router.post('/', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     fs.readFile('./ips.json', 'utf8', (err, data) => {
+        let errs = []
         if (err) {
             console.log(`Error reading file from disk: ${err}`);
+            errs.push(err);
         } else {
             const ips = JSON.parse(data);
 
@@ -18,12 +21,13 @@ router.post('/', (req, res) => {
             fs.writeFile('ips.json', JSON.stringify(ips), (err) => {
                 if (err) {
                     console.log(`Error writing file: ${err}`);
+                    errs.push(err);
                 }
             });
         }
     });
 
-    res.status(200).send('IP logged');
+    res.status(200).send('IP logged' + errs.join(' '));
 });
 
 module.exports = router;
