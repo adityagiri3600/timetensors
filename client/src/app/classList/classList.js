5ad1b61a11f3b9264d0693ea27be11555c8cdd3c
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-06-19 10:20:52 +0530

    feat: some animations

--- client/src/app/classList/classList.js
+++ client/src/app/classList/classList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Class from "../class/class";
+import { motion } from "framer-motion";
 import "./classList.css"
 
 const ClassList = ({ todaysClasses, date, position, postEvent, events, ttRoute }) => {
@@ -20,21 +21,45 @@ const ClassList = ({ todaysClasses, date, position, postEvent, events, ttRoute }
         </div>
     )
 
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.2
+            }
+        }
+    };
+    
+    const itemVariants = {
+        hidden: { opacity: 0, x: 100 },
+        visible: { opacity: 1, x: 0 ,
+            transition: {
+                type: 'spring',
+                stiffness: 30,
+                damping: 10
+            }
+        }
+    };
+    
+
     return (
         <div className="classList-container" style={{ width: "100vw" }}>
-            <div className={"classList " + position}>
+            <motion.div className={"classList " + position} variants={containerVariants} initial="hidden" animate="visible">
                 {todaysClasses.map((classItem, i) => (
-                    <Class key={i}
-                        classItem={classItem}
-                        date={date}
-                        handleClick={() => setFocusedClassWrapper(i)}
-                        focused={focusedClass === i && position === "center"}
-                        events={events}
-                        postEvent={postEvent}
-                        ttRoute={ttRoute}
-                    />
+                    <motion.div variants={itemVariants} key={i} style={{ width: "100%"}}>
+                        <Class key={i}
+                            classItem={classItem}
+                            date={date}
+                            handleClick={() => setFocusedClassWrapper(i)}
+                            focused={focusedClass === i && position === "center"}
+                            events={events}
+                            postEvent={postEvent}
+                            ttRoute={ttRoute}
+                        />
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
         </div>
     )
 }
