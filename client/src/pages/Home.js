66e74809442a1256c4c260dafd7fc3f8b2824323
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-06-24 14:57:48 +0530

    feat: github link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,12 @@ import { useAuth } from "../AuthContext";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { motion } from "framer-motion";
-import { IconTablePlus } from "@tabler/icons-react";
+import {
+    IconBrandGithub,
+    IconLogout,
+    IconTablePlus,
+    IconUser,
+} from "@tabler/icons-react";
 import "./Home.css";
 
 const Home = () => {
@@ -51,7 +56,6 @@ const Home = () => {
         <motion.div
             style={{
                 height: "100vh",
-                overflow: "hidden",
             }}
         >
             <div
@@ -63,9 +67,11 @@ const Home = () => {
                 <div
                     style={{
                         display: "flex",
-                        justifyContent: "center",
+                        justifyContent: "space-between",
                         alignItems: "center",
                         paddingTop: "20px",
+                        paddingLeft: "20px",
+                        paddingRight: "20px",
                     }}
                 >
                     <div
@@ -94,8 +100,25 @@ const Home = () => {
                         </p>
                     </div>
                     {isLoggedIn ? (
-                        <>
-                            <p>{userData.username}</p>
+                        <div
+                            style={{
+                                border: "1px solid #FFFFFFAA",
+                                borderRadius: "5px",
+                                display: "flex",
+                                alihnitItems: "center",
+                            }}
+                        >
+                            <p
+                                style={{
+                                    margin: "10px",
+                                    display: "flex",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <IconUser size={20} />
+                                &nbsp;
+                                {userData.username}
+                            </p>
                             <button
                                 onClick={() => {
                                     logout();
@@ -103,17 +126,16 @@ const Home = () => {
                                 }}
                                 className="btn-press"
                                 style={{
-                                    background: "none",
+                                    background: "#f31e40",
                                     color: "white",
-                                    border: "1px solid #FFFFFFAA",
+                                    border: "none",
                                     padding: "10px",
-                                    borderRadius: "5px",
-                                    marginLeft: "10px",
+                                    borderRadius: "0  5px 5px 0",
                                 }}
                             >
-                                Log out
+                                <IconLogout size={20} />
                             </button>
-                        </>
+                        </div>
                     ) : (
                         <Link
                             onClick={() => {
@@ -181,7 +203,14 @@ const Home = () => {
                                         maxWidth: "300px",
                                     }}
                                 >
-                                    <motion.div className="textFade" initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 0.5 } }}>
+                                    <motion.div
+                                        className="textFade"
+                                        initial={{ opacity: 0 }}
+                                        animate={{
+                                            opacity: 1,
+                                            transition: { duration: 0.5 },
+                                        }}
+                                    >
                                         <div
                                             style={{
                                                 display: "flex",
@@ -220,6 +249,51 @@ const Home = () => {
                         })}
                     </div>
                 </>
+                <div
+                    style={{
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        position: "absolute",
+                        bottom: "10px",
+                    }}
+                >
+                    <Link
+                        to={"https://github.com/adityagiri3600/timetensors"}
+                        target="_blank"
+                        style={{
+                            backgroundColor: "#010409",
+                            padding: "10px",
+                            borderRadius: "5px",
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <svg
+                            height="32"
+                            aria-hidden="true"
+                            viewBox="0 0 16 16"
+                            version="1.1"
+                            width="20"
+                            data-view-component="true"
+                            class="octicon octicon-mark-github v-align-middle color-fg-default"
+                        >
+                            <path
+                                fill="white"
+                                d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+                            ></path>
+                        </svg>
+                        &nbsp; github
+                    </Link>
+                    <img
+                        src="/starit.png"
+                        style={{
+                            width: "150px",
+                        }}
+                    />
+                </div>
             </div>
         </motion.div>
     );
