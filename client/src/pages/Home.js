c790c566f96d64ddfe47c455ad01b66ca8d5c85b
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-06-25 14:21:39 +0530

    feat: glow button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -260,33 +260,201 @@ const Home = () => {
                         bottom: "10px",
                     }}
                 >
-                    <Link
-                        to={"https://github.com/adityagiri3600/timetensors"}
-                        target="_blank"
-                        style={{
-                            backgroundColor: "#010409",
-                            padding: "10px",
-                            borderRadius: "5px",
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        <svg
-                            height="32"
-                            aria-hidden="true"
-                            viewBox="0 0 16 16"
-                            version="1.1"
-                            width="20"
-                            data-view-component="true"
-                            class="octicon octicon-mark-github v-align-middle color-fg-default"
+                    <div>
+                        <div
+                            style={{
+                                filter: "blur(10px)",
+                                position: "absolute",
+                                zIndex: "-1",
+                                opacity: "0.5",
+                            }}
                         >
-                            <path
-                                fill="white"
-                                d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
-                            ></path>
+                            <svg
+                                style={{
+                                    position: "absolute",
+                                    zIndex: "-1",
+                                    width: "99px",
+                                    height: "40px",
+                                    borderRadius: "5px",
+                                }}
+                            >
+                                <defs>
+                                    <radialGradient id="redGradient">
+                                        <stop offset="0%" stopColor="#E32636" />
+                                        <stop offset="100%" stopColor="black" />
+                                    </radialGradient>
+                                    <radialGradient id="blueGradient">
+                                        <stop offset="0%" stopColor="#00BFFF" />
+                                        <stop offset="100%" stopColor="black" />
+                                    </radialGradient>
+                                    <filter
+                                        id="blurFilter"
+                                        x="0"
+                                        y="0"
+                                        width="100%"
+                                        height="100%"
+                                    >
+                                        <feGaussianBlur stdDeviation="5" />
+                                    </filter>
+                                    <style>
+                                        {`
+            @keyframes moveRed {
+                0% { cx: 100%; cy: 100%; }
+                25% { cx: 0%; cy: 100%; }
+                50% { cx: 0%; cy: 0%; }
+                75% { cx: 100%; cy: 0%; }
+                100% { cx: 100%; cy: 100%; }
+            }
+            @keyframes moveBlue {
+                0% { cx: 0%; cy: 0%; }
+                25% { cx: 100%; cy: 0%; }
+                50% { cx: 100%; cy: 100%; }
+                75% { cx: 0%; cy: 100%; }
+                100% { cx: 0%; cy: 0%; }
+            }
+            #redCircle {
+              animation: moveRed 8s infinite;
+              filter: saturate(1);
+            }
+            #blueCircle {
+              animation: moveBlue 8s infinite;
+              filter: saturate(1);
+            }
+          `}
+                                    </style>
+                                </defs>
+
+                                <g
+                                    filter="url(#blurFilter)"
+                                    clipPath="url(#textClip)"
+                                >
+                                    <circle
+                                        id="redCircle"
+                                        cx="50"
+                                        cy="50"
+                                        r="50"
+                                        fill="url(#redGradient)"
+                                        style={{ mixBlendMode: "screen" }}
+                                    />
+                                    <circle
+                                        id="blueCircle"
+                                        cx="50"
+                                        cy="50"
+                                        r="50"
+                                        fill="url(#blueGradient)"
+                                        style={{ mixBlendMode: "screen" }}
+                                    />
+                                </g>
+                            </svg>
+                        </div>
+                        <svg
+                            style={{
+                                position: "absolute",
+                                zIndex: "-1",
+                                width: "99px",
+                                height: "40px",
+                                borderRadius: "5px",
+                            }}
+                        >
+                            <defs>
+                                <radialGradient id="redGradient">
+                                    <stop offset="0%" stopColor="#E32636" />
+                                    <stop offset="100%" stopColor="black" />
+                                </radialGradient>
+                                <radialGradient id="blueGradient">
+                                    <stop offset="0%" stopColor="#00BFFF" />
+                                    <stop offset="100%" stopColor="black" />
+                                </radialGradient>
+                                <filter
+                                    id="blurFilter"
+                                    x="0"
+                                    y="0"
+                                    width="100%"
+                                    height="100%"
+                                >
+                                    <feGaussianBlur stdDeviation="5" />
+                                </filter>
+                                <style>
+                                    {`
+            @keyframes moveRed {
+                0% { cx: 100%; cy: 100%; }
+                25% { cx: 0%; cy: 100%; }
+                50% { cx: 0%; cy: 0%; }
+                75% { cx: 100%; cy: 0%; }
+                100% { cx: 100%; cy: 100%; }
+            }
+            @keyframes moveBlue {
+                0% { cx: 0%; cy: 0%; }
+                25% { cx: 100%; cy: 0%; }
+                50% { cx: 100%; cy: 100%; }
+                75% { cx: 0%; cy: 100%; }
+                100% { cx: 0%; cy: 0%; }
+            }
+            #redCircle {
+              animation: moveRed 8s infinite;
+              filter: saturate(1);
+            }
+            #blueCircle {
+              animation: moveBlue 8s infinite;
+              filter: saturate(1);
+            }
+          `}
+                                </style>
+                            </defs>
+
+                            <g
+                                filter="url(#blurFilter)"
+                                clipPath="url(#textClip)"
+                            >
+                                <circle
+                                    id="redCircle"
+                                    cx="50"
+                                    cy="50"
+                                    r="50"
+                                    fill="url(#redGradient)"
+                                    style={{ mixBlendMode: "screen" }}
+                                />
+                                <circle
+                                    id="blueCircle"
+                                    cx="50"
+                                    cy="50"
+                                    r="50"
+                                    fill="url(#blueGradient)"
+                                    style={{ mixBlendMode: "screen" }}
+                                />
+                            </g>
                         </svg>
-                        &nbsp; github
-                    </Link>
+
+                        <Link
+                            to={"https://github.com/adityagiri3600/timetensors"}
+                            target="_blank"
+                            style={{
+                                margin: "2px",
+                                backgroundColor: "#010409da",
+                                padding: "2px 10px",
+                                borderRadius: "5px",
+                                display: "flex",
+                                alignItems: "center",
+                                backdropFilter: "blur(1px)",
+                            }}
+                        >
+                            <svg
+                                height="32"
+                                aria-hidden="true"
+                                viewBox="0 0 16 16"
+                                version="1.1"
+                                width="20"
+                                data-view-component="true"
+                                class="octicon octicon-mark-github v-align-middle color-fg-default"
+                            >
+                                <path
+                                    fill="white"
+                                    d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+                                ></path>
+                            </svg>
+                            &nbsp; github
+                        </Link>
+                    </div>
                     <img
                         src="/starit.png"
                         style={{
