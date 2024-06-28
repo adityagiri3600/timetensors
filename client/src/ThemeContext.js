4cf43f8a41816e00532234291fd2a41dd39e9b2e
Aditya Giri <74224708+adityagiri3600@users.noreply.github.com>
2024-06-28 11:16:26 +0530

    fix: theme localstorage

--- client/src/ThemeContext.js
+++ client/src/ThemeContext.js
@@ -3,18 +3,17 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('classic');
-
-    useEffect(() => {
+    const [theme, setTheme] = useState(() => {
         const localTheme = localStorage.getItem('theme');
-        if (localTheme) {
-            setTheme(localTheme);
-        }
-    }, []);
+        return localTheme ? localTheme : 'classic';
+    });
 
     useEffect(() => {
         document.documentElement.style.setProperty('--foreground-rgb', theme === 'paper' ? '0, 0, 0' : '255, 255, 255');
         document.documentElement.style.setProperty('--background-rgb', theme === 'paper' ? '243, 232, 213' : '0, 0, 0');
+        if (window !== undefined) {
+            localStorage.setItem('theme', theme);
+        }
     }, [theme]);
 
     const value = {
